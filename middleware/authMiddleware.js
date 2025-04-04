e7564e1cc87af8073f7aa80e644ce169f6f2848f
shivasaireddy <shivasaireddyp@gmail.com>
2025-04-05 00:41:01 +0530

    changed some lines of code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ import User from "../models/userModel.js";
 const protectRoute = asyncHandler(async (req, res, next) => {
   // let token = req.cookies.token;
   let token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-
+  console.log("here is the token came:",token)
   if (token) {
     try {
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
