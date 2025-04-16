47a24c5d4ee4fec445be01199fa4392b34bc25f1
shivasaireddy <shivasaireddyp@gmail.com>
2025-04-16 20:27:04 +0530

    changed server/.env in render and samesite to none

--- utils/index.js
+++ utils/index.js
@@ -7,8 +7,9 @@ const createJWT = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "development", // Use secure cookies in production
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // Prevent CSRF attacks
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // Prevent CSRF attacks
+    sameSite: "None", // Prevent CSRF attacks
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
