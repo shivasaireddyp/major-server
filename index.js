02b69f84b2b795d129084f7b75301254ecb27a3a
shivasaireddy <shivasaireddyp@gmail.com>
2025-04-16 23:16:40 +0530

    updated frontend url in .env

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import bcrypt from "bcryptjs";
 import { errorHandler, routeNotFound } from "./middleware/errorMiddleware.js";
 import routes from "./routes/index.js";
 import dbConnection from "./utils/connectDB.js";
@@ -16,15 +15,6 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 5000;
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "http://localhost:3001"],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
 
 app.options("*", cors());
 
